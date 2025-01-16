6ff3cfa8888a4a34be0e096cf03a610b9b41b1c8
Marcelo Vesantera <mrvesantera@hotmail.com>
2025-01-15 23:41:49 -0300

    ajustes css

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,26 +1,50 @@
 "use client";
 
-import LoginForm from "@/app/components/Auth/LoginForm";
+// import LoginForm from "@/app/Components/Auth/LoginForm";
 import styles from "./page.module.css";
+import Image from "next/image";
+import login_image from "@/app/Images/groceries_login.png";
 
 const LoginPage = () => {
-  const handleLogin = (email: string, password: string) => {
-    console.log("Login attempt:", { email, password });
-  };
+  // const handleLogin = (email: string, password: string) => {
+  //   console.log("Login attempt:", { email, password });
+  // };
 
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.formSection}>
-        <h1 className={styles.heading}>Bem-vindo ao Weekly Groceries!</h1>
-        <p>
-          Já tem uma conta?{" "}
-          <a className={styles.link} href="/auth/login">
-            Entrar
-          </a>
-        </p>
-        <LoginForm onSubmit={handleLogin} />
+    <div className={styles.page}>
+      <div className={styles.container}>
+        <div className={styles.login_img}>
+          <Image className={styles.cover} src={login_image} alt="Login" />
+        </div>
+        <div className={styles.form_div}>
+          <div className={styles.text_div}>
+            <h4 className={styles.text}>Bem-vindo ao Weekly Groceries!</h4>
+          </div>
+
+          <div className={styles.items_div}>
+            <input
+              className={styles.input}
+              type="email"
+              aria-describedby="emailHelp"
+              placeholder="Digite o seu email..."
+              name="email"
+            />
+          </div>
+          <div className={styles.items_div}>
+            <input
+              className={styles.input}
+              type="password"
+              placeholder="Senha"
+              name="senha"
+            />
+          </div>
+          <div className={styles.items_div}>
+            <button className={styles.btn_login} type="submit">
+              Entrar
+            </button>
+          </div>
+        </div>
       </div>
-      <div className={styles.imageSection}></div>
     </div>
   );
 };
