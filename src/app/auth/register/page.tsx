d220b84591fc4e02b5c0898c8766476282587029
Marcelo Vesantera <mrvesantera@hotmail.com>
2025-01-19 18:49:14 -0300

    tela de registro

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -2,11 +2,10 @@
 
 import styles from "./page.module.css";
 import { useState } from "react";
-import Link from "next/link";
 import InputField from "@/app/Components/Auth/input-field";
 import SubmitBtn from "@/app/Components/Auth/submit-btn";
 
-const LoginPage = () => {
+const RegisterPage = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const emailType = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -24,8 +23,8 @@ const LoginPage = () => {
     return true;
   };
 
-  const onHandleLogin = (email: string, password: string) => {
-    console.log("Login attempt:", { email, password });
+  const onHandleRegister = (email: string, password: string) => {
+    console.log("Register attempt:", { email, password });
 
     if (email === "" || password === "") {
       console.log("Email e senha são obrigatórios");
@@ -36,7 +35,7 @@ const LoginPage = () => {
       onValidateInput(email, "email") &&
       onValidateInput(password, "password")
     ) {
-      console.log("Login efetuado com sucesso");
+      console.log("Registro efetuado com sucesso");
     } else {
       console.log("Email ou senha inválidos");
     }
@@ -48,51 +47,46 @@ const LoginPage = () => {
         <div className={styles.form_div}>
           <div className={styles.text_div}>
             <h4 className={styles.text}>
-              Bem-vindo ao <b>Weekly Groceries!</b>
+              Cadastre-se no <b>Weekly Groceries!</b>
             </h4>
           </div>
 
           <div className={styles.items_div}>
+            <span className={styles.input_text}>Nome:</span>
+            <InputField type="text" name="name" setValue={setEmail} />
+          </div>
+          <div className={styles.items_div}>
+            <span className={styles.input_text}>Sobrenome:</span>
+            <InputField type="text" name="lastname" setValue={setEmail} />
+          </div>
+          <div className={styles.items_div}>
+            <span className={styles.input_text}>Email:</span>
             <InputField
               type="email"
               described="emailHelp"
-              placeholder="Digite o seu email..."
               name="email"
               setValue={setEmail}
             />
           </div>
           <div className={styles.items_div}>
-            <InputField
-              type="password"
-              placeholder="Senha"
-              name="senha"
-              setValue={setPassword}
-            />
+            <span className={styles.input_text}>Senha:</span>
+            <InputField type="password" name="senha" setValue={setPassword} />
+          </div>
+          <div className={styles.items_div}>
+            <span className={styles.input_text}>Confirme sua senha:</span>
+            <InputField type="password" name="senha" setValue={setPassword} />
           </div>
 
           <div className={styles.items_div}>
             <SubmitBtn
-              text="Entrar"
-              onClick={() => onHandleLogin(email, password)}
+              text="Registrar"
+              onClick={() => onHandleRegister(email, password)}
             />
           </div>
-
-          <p className={styles.text_link}>
-            Não tem uma conta?{" "}
-            <Link className={styles.link} href="/auth/register">
-              Cadastre-se
-            </Link>
-          </p>
-          <p className={styles.text_link}>
-            Esqueceu sua senha?{" "}
-            <Link className={styles.link} href="/auth/forgot-password">
-              Recuperar senha
-            </Link>
-          </p>
         </div>
       </div>
     </div>
   );
 };
 
-export default LoginPage;
+export default RegisterPage;
